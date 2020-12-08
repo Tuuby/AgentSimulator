9f576496ebe72bbbc5cd5d8a356be37b77a14831
Tobias <tobiastrompell@aol.com>
2020-12-08 01:08:44 +0100

    Add inline comments to world.java

--- src/main/java/world/World.java
+++ src/main/java/world/World.java
@@ -2,22 +2,27 @@ package world;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+// Prototype class that represents the world and all the GameObjects on it
 public class World {
 
+    // List of GameObjects that works well for multithreading
     private static ConcurrentLinkedQueue<GameObject> gameObjects = new ConcurrentLinkedQueue<GameObject>();
 
+    // Method to call update for all the GameObjects in this world
     public static void update() {
         for (GameObject go : gameObjects) {
             go.update();
         }
     }
 
+    // Method to call render for all the GameObjects in this world
     public static void render() {
         for (GameObject go : gameObjects) {
             go.render();
         }
     }
 
+    // Method to add a new GameObject to the list during the Runtime
     public static void addObject(GameObject go) {
         gameObjects.offer(go);
     }
