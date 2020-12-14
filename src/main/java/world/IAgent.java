0cf9757d2e7e1d814048cfb17204c27827363af6
Tobias <tobiastrompell@aol.com>
2020-12-14 14:58:00 +0100

    Add IAgent interface

---
+++ src/main/java/world/IAgent.java
@@ -0,0 +1,10 @@
+package world;
+
+// Interface for the Agents
+public interface IAgent {
+    void setName(String name);
+    String getName();
+
+    // TODO: figure out what a performative is
+    //void performKQML(IAgent sender, Performative performative);
+}
