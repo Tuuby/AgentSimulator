10f4be394084e406a09bbaa2870eef4072e13cdc
Tobias <tobiastrompell@aol.com>
2020-12-14 13:21:21 +0100

    Add Hill GameObject

--- src/main/java/world/Hill.java
+++ src/main/java/world/Hill.java
@@ -1,18 +1,25 @@
 package world;
 
+import graphics.Graphics;
+
+// Class to represent to hills on the world than block pathing and vision I think
 public class Hill extends GameObject {
 
+    // Radius of the hill
     private float radius;
 
+    // Constructor to make a new GameObject with a radius
     public Hill(World w, int x, int y, float r) {
         super(x, y, w);
         radius = r;
     }
 
+    // Method to check the radius
     public float getRadius() {
         return radius;
     }
 
+    // toString method to output the hill as a String
     @Override
     public String toString() {
         return "Hill{" +
@@ -23,19 +30,19 @@ public class Hill extends GameObject {
                 '}';
     }
 
+    // Inherited method to check if the hill reaches to a specific point
     public boolean extendsTo(int dx, int dy) {
         return (dx * dx + dy * dy < radius * radius);
     }
 
-    public void moveTo(int xabs, int yabs) {
-
-    }
-
+    // Method to update the hill every gameupdate tick
     public void update() {
-
+        // Probably not much to do here, as a hill just hangs around
     }
 
+    // Method to render the hill ever frame
     public void render() {
-
+        Graphics.setColor(0.54f, 0.27f, 0.07f, 1);
+        Graphics.fillCircle(x, y, radius);
     }
 }
