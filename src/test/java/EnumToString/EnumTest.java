33b056e0e47728c8875e04dfcad50912af7e83c7
Tobias <tobiastrompell@aol.com>
2020-12-23 18:57:35 +0100

    Add Small class to test String output of an Enumeration

---
+++ src/test/java/EnumToString/EnumTest.java
@@ -0,0 +1,9 @@
+package EnumToString;
+
+import world.enums.AgentActions;
+
+public class EnumTest {
+    public static void main(String[] args) {
+        System.out.println(AgentActions.REPRODUCE.name());
+    }
+}
