4c3c2e083f46e2bb1a088bbb521ac3e1086dd82c
Tobias <tobiastrompell@aol.com>
2020-12-25 14:31:03 +0100

    Remove EnumTest as it was not really helpful

--- src/test/java/EnumToString/EnumTest.java
+++
@@ -1,9 +0,0 @@
-package EnumToString;
-
-import world.enums.AgentActions;
-
-public class EnumTest {
-    public static void main(String[] args) {
-        System.out.println(AgentActions.REPRODUCE.name());
-    }
-}
