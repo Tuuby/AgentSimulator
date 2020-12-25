5a5041a021c020f50043a8d6621b4dd652d497c8
Tobias <tobiastrompell@aol.com>
2020-12-25 15:24:10 +0100

    Remove one TODO in IFood

--- src/main/java/world/IFood.java
+++ src/main/java/world/IFood.java
@@ -2,7 +2,6 @@ package world;
 
 // Interface for food gameobjects to include energy functions
 public interface IFood {
-    //TODO: check if more methods are needed
     int getEnergy();
     void removeEnergy(int de);
 }
